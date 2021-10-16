3336217e39fcd8e86750eec50ba9408dad3b61c1
Jeferson Amorim <jeferson.amorim@ceros.com>
2021-10-16 08:09:01 -0300

    Fix redoc options

--- src/interfaces/redoc-try-it-out-options.interface.ts
+++ src/interfaces/redoc-try-it-out-options.interface.ts
@@ -4,78 +4,217 @@ import { TryBtnOptions } from './try-btn-options.interface';
 import { StyleMatcherOptions } from './style-matcher-options.interface';
 
 export interface RedocOptions {
-  redocVersion?: string;
-  /** Web site title (e.g: ReDoc documentation) */
-  title?: string;
-  /** Web site favicon URL */
-  favicon?: string;
-  /** Logo Options */
-  logo?: LogoOptions;
-  /** Theme options */
-  theme?: any;
-  /** If set, the spec is considered untrusted and all HTML/markdown is sanitized to prevent XSS, by default is false */
-  untrustedSpec?: boolean;
-  /** If set, warnings are not rendered at the top of documentation (they are still logged to the console) */
-  supressWarnings?: boolean;
-  /** If set, the protocol and hostname won't be shown in the operation definition */
-  hideHostname?: boolean;
-  /** Specify which responses to expand by default by response codes,
-   * values should be passed as comma-separated list without spaces
-   * (e.g: 200, 201, "all")
-   */
-  expandResponses?: string;
-  /** If set, show required properties first ordered in the same order as in required array */
-  requiredPropsFirst?: boolean;
-  /** If set, propeties will be sorted alphabetically */
-  sortPropsAlphabetically?: boolean;
-  /** If set the fields starting with "x-" will be shown, can be a boolean or a string with names of extensions to display */
-  showExtensions?: boolean | string;
-  /** If set, redoc won't inject authentication section automatically */
-  noAutoAuth?: boolean;
-  /** If set, path link and HTTP verb will be shown in the middle panel instead of the right one */
-  pathInMiddlePanel?: boolean;
-  /** If set, loading spinner animation won't be shown */
-  hideLoading?: boolean;
-  /** If set, a native scrollbar will be used instead of perfect-scroll, this can improve performance of the frontend for big specs */
-  nativeScrollbars?: boolean;
-  /** This will hide the "Download spec" button, it only hides the button */
-  hideDownloadButton?: boolean;
-  /** If set, the search bar will be disabled */
+
+  /** disable search indexing and search box */
   disableSearch?: boolean;
-  /** Shows only required fileds in request samples */
+  /** enable expanding default server variables, default false.*/
+  expandDefaultServerVariables?: boolean;
+  /** specify which responses to expand by default by response codes.
+   * Values should be passed as comma-separated list without spaces e.g. expandResponses="200,201".
+   * Special value "all" expands all responses by default.
+   * Be careful: this option can slow-down documentation rendering time.
+   **/
+  expandResponses?: boolean;
+  /** set the maximum render depth for JSON payload samples (responses and request body). The default value is 10.*/
+  generatedPayloadSamplesMaxDepth?: number;
+  /** display only specified number of enum values. hide rest values under spoiler. */
+  maxDisplayedEnumValues?: number;
+  /** do not show "Download" spec button. THIS DOESN'T MAKE YOUR SPEC PRIVATE, it just hides the button. */
+  hideDownloadButton?: boolean;
+  /** if set, the protocol and hostname is not shown in the operation definition. */
+  hideHostname?: boolean;
+  /** do not show loading animation. Useful for small docs. */
+  hideLoading?: boolean;
+  /** if set, the pattern is not shown in the schema. */
+  hideSchemaPattern?: boolean;
+  /** do not show the request sample tab for requests with only one sample. */
+  hideSingleRequestSampleTab?: boolean;
+  /** automatically expand single field in a schema */
+  expandSingleSchemaField?: boolean;
+  /** set the default expand level for JSON payload samples (responses and request body).
+   * Special value "all" expands all levels.
+   * The default value is 2.
+   **/
+  jsonSampleExpandLevel?: number;
+  /** do not display schema title next to to the type */
+  hideSchemaTitles?: boolean;
+  /** show only unique oneOf types in the label without titles */
+  simpleOneOfTypeLabel?: boolean;
+  /** Not implemented yet if set, enables lazy rendering mode in ReDoc.
+   * This mode is useful for APIs with big number of operations (e.g. > 50).
+   * In this mode ReDoc shows initial screen ASAP and then renders the rest
+   * operations asynchronously while showing progress bar on the top.
+   * Check out the demo for the example.
+   **/
+  lazyRendering?: boolean;
+  /** if true clicking second time on expanded menu item will collapse it, default true. */
+  menuToggle?: boolean;
+  /** use native scrollbar for sidemenu instead of perfect-scroll (scrolling performance optimization for big specs). */
+  nativeScrollbars?: boolean;
+  /** do not inject Authentication section automatically. */
+  noAutoAuth?: boolean;
+  /** shows only required fields in request samples. */
   onlyRequiredInSamples?: boolean;
-  /** Name of the swagger json spec file */
-  docName?: string;
-  /** Authentication options */
-  auth?: {
-    // Default value is false
-    enabled: boolean;
-    // If auth is enabled but no user is provided the default value is "admin"
-    user: string;
-    // If auth is enabled but no password is provided the default value is "123"
-    password: string;
+  /** show path link and HTTP verb in the middle panel instead of the right one. */
+  pathInMiddlePanel?: boolean;
+  /** show required properties first ordered in the same order as in required array. */
+  requiredPropsFirst?: boolean;
+  /** If set, specifies a vertical scroll-offset.
+   * This is often useful when there are fixed positioned elements at the top of the page, such as navbars, headers etc;
+   * scrollYOffset can be specified in various ways: number: A fixed number of pixels to be used as offset.
+   **/
+  scrollYOffset?: number|string;
+  /** selector of the element to be used for specifying the offset.
+   * The distance from the top of the page to the element's bottom will be used as offset.
+   **/
+  selector?: string;
+  /** A getter function. Must return a number representing the offset (in pixels). */
+  function?: () => number;
+  /** show vendor extensions ("x-" fields).
+   * Extensions used by ReDoc are ignored. Can be boolean or an array of string with names of extensions to display.
+   **/
+  showExtensions?: boolean|Array<string>;
+  /** sort properties alphabetically. */
+  sortPropsAlphabetically?: boolean;
+  /** if set, payload sample will be inserted at this index or last. Indexes start from 0.*/
+  payloadSampleIdx?: number;
+  /** ReDoc theme. For details check theme docs. */
+  theme?: ThemOptions;
+  /** if set, the spec is considered untrusted and all HTML/markdown is sanitized to prevent XSS.
+   * Disabled by default for performance reasons. Enable this option if you work with untrusted user data!
+   **/
+  untrustedSpec?: boolean;
+}
+
+export interface ThemOptions {
+
+  spacing?: {
+    /** main spacing unit used in autocomputed theme values later - default 5 */
+    unit?: number,
+    /** Horizontal section padding. COMPUTED: spacing.unit * 8 */
+    sectionHorizontal?: number,
+    /** Horizontal section padding. COMPUTED: spacing.unit * 8 */
+    sectionVertical?: number
   };
 
-  /** Vendor extensions */
+  /** breakpoints for switching three/two and mobile view layouts */
+  breakpoints?: {
+    /** '50rem' */
+    small?: string,
+    /** '85rem' */
+    medium?: string,
+    /** '105rem' */
+    large?: string
+  }
 
-  /** If set, group tags in categories in the side menu. Tags not added to a group will not be displayed. */
-  tagGroups?: TagGroupOptions[];
-}
+  colors?: {
+    /** default tonal offset used in computations - default 0.3 */
+    tonalOffset?: number
+  }
 
-export interface LogoOptions {
-  /** The URL pointing to the spec logo, must be in the format of a URL and an absolute URL */
-  url?: string;
-  /** Background color to be used, must be RGB color in hexadecimal format (e.g: #008080) */
-  backgroundColor?: string;
-  /** Alt tag for logo */
-  altText?: string;
-  /** href tag for logo, it defaults to the one used in your API spec */
-  href?: string;
-}
+  typography?: {
+    /** '14px' */
+    fontSize?: string,
+    /** '1.5em' */
+    lineHeight?: string,
+    /** 400 */
+    fontWeightRegular?: number,
+    /** 600 */
+    fontWeightBold?: number,
+    /** 300 */
+    fontWeightLight?: number,
+    /** 'Roboto, sans-serif' */
+    fontFamily?: string,
+    /** 'antialiased' */
+    smoothing?: string,
+    /** true */
+    optimizeSpeed?: boolean
 
-export interface TagGroupOptions {
-  name: string;
-  tags: string[];
+    headings?: {
+      /** 'Montserrat, sans-serif' */
+      fontFamily?: string,
+      /** 400 */
+      fontWeight?: number,
+      /** '1.6em' */
+      lineHeight?: string
+    }
+
+    /** inline code styling */
+    code?: {
+      /** '13px' */
+      fontSize?: string,
+      /** 'Courier, monospace' */
+      fontFamily?: string,
+      /** COMPUTED: typography.lineHeight */
+      lineHeight?: number,
+      /** COMPUTED: typography.fontWeightRegular */
+      fontWeight?: number,
+      /** '#e53935' */
+      color?: string,
+      /** 'rgba(38, 50, 56, 0.05)' */
+      backgroundColor?: string,
+      /** whether to break word for inline blocks (otherwise they can overflow) - default: false */
+      wrap?: boolean
+    }
+
+    links?: {
+      /** COMPUTED: colors.primary.main */
+      color?: string,
+      /** COMPUTED: typography.links.color */
+      visited?: string,
+      /** COMPUTED: lighten(0.2 typography.links.color) */
+      hover?: string
+    }
+  }
+
+  menu?: {
+    /** '260px' */
+    width?: string,
+    /** '#fafafa' */
+    backgroundColor?: string,
+    /** '#333333' */
+    textColor?: string,
+    /** COMPUTED: theme.menu.textColor (if set by user) or theme.colors.primary.main */
+    activeTextColor?: string,
+
+    /** Group headings */
+    groupItems?: { //
+      /** 'uppercase' */
+      textTransform?: string,
+    }
+
+    /** Level 1 items like tags or section 1st level items */
+    level1Items?: {
+      /** 'none' */
+      textTransform?: string
+    }
+
+    /** menu arrow */
+    arrow?: {
+      /** '1.5em' */
+      size?: string,
+      /** COMPUTED: theme.menu.textColor */
+      color?: string
+    }
+  }
+
+  logo?: {
+    /** COMPUTED: menu.width */
+    maxHeight?: string,
+    /** COMPUTED: menu.width */
+    maxWidth?: string,
+    /** logo image padding - default: '2px' */
+    gutter?: string
+  }
+
+  rightPanel?: {
+    /** '#263238' */
+    backgroundColor?: string,
+    /** '40%' */
+    width?: string,
+    /** '#ffffff' */
+    textColor?: string
+  }
 }
 
 export interface DependenciesVersions {
@@ -84,6 +223,7 @@ export interface DependenciesVersions {
 }
 
 export interface RedocTryItOutOptions extends RedocOptions {
+  redocVersion?: string;
   tryItOutEnabled?: boolean;
   tryItBoxContainerId?: string;
   containerId?: string;
